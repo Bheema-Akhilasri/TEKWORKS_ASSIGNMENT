94df968b9fba1b928647e79c4bb898f535869473
Bheema-Akhilasri <akhilasribheema68@gmail.com>
2024-12-13 13:36:58 +0530

    Create shopping.py

---
+++ 12-12-2024/shopping.py
@@ -0,0 +1,9 @@
+#salary ,3 shopping bills,total shopping, percentage of shopping in salary
+salary=int(input('Enter salary'))
+shop1=int(input('amount in shop 1'))
+shop2=int(input('amount in shop 2'))
+shop3=int(input('amount in shop 3'))
+total= shop1+shop2+shop3
+print(total)
+percentage=(total/salary)*100
+print(percentage)

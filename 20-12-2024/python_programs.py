ab935693d81d680a17249bf34bc8cf47799e2413
Bheema-Akhilasri <akhilasribheema68@gmail.com>
2024-12-21 09:38:25 +0530

    Update python_programs.py

--- 20-12-2024/python_programs.py
+++ 20-12-2024/python_programs.py
@@ -17,4 +17,38 @@ def check_password_strength(password):
 password=input('Enter your password : ')
 print(check_password_strength(password))
 
+#Factorial 
+def factorial(num):
+    result=1
+    for i in range(1,num+1):
+        result*= i
+    return result
+fact=int(input('Enter number:'))
+print(f"Factorial of {fact} is",factorial(fact))
+
+#Prime Number
+def prime(num):
+    count=0
+    if num==0 or num==1:
+        return "neither prime nor composite"
+    for i in range(1,num+1):
+        if num%i==0:
+            count+=1
+    if count>2:
+        return "not prime"
+    else:
+        return "prime"
+number=int(input('Enter number:'))
+print(f"The number {number} is",prime(number))
+
+
+#Leap Year
+def leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return "leap year"
+    else:
+        return "not a leap year"
+year=int(input('Enter year:'))
+print(f"The year {year} is",leap_year(year))
+
 #

dbf354174b78dbcf19b2f4de7b50c51873478ca8
Bheema-Akhilasri <akhilasribheema68@gmail.com>
2024-12-23 21:24:14 +0530

    Create Convert_distance.py

---
+++ Python_course_questions/Convert_distance.py
@@ -0,0 +1,12 @@
+inches = 12
+yards = 1/3
+miles = 1/5280
+feet = float(input("Enter the distance in feet: "))
+inches = feet*inches
+yards = feet*yards
+miles = feet*miles
+
+print(f"Distance in feet: {feet}")
+print(f"Distance in inches: {inches}")
+print(f"Distance in yards: {yards}")
+print(f"Distance in miles: {miles}")
